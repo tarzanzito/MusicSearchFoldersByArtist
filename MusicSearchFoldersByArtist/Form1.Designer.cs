e8bd9480d4109aabebbae00b15d408979a48ee04
tarzanzito <tarzanzito@gmail.com>
2023-04-27 19:18:53 +0100

    add files

---
+++ MusicSearchFoldersByArtist/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MusicFindArtist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxArtist = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Artist";
+            // 
+            // textBoxArtist
+            // 
+            this.textBoxArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxArtist.Location = new System.Drawing.Point(48, 13);
+            this.textBoxArtist.Name = "textBoxArtist";
+            this.textBoxArtist.Size = new System.Drawing.Size(360, 21);
+            this.textBoxArtist.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.Location = new System.Drawing.Point(486, 8);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(56, 29);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(18, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(462, 139);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "name*",
+            "*name*"});
+            this.comboBox1.Location = new System.Drawing.Point(414, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(486, 153);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(56, 29);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(554, 194);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxArtist);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search folders by \'Artist\'";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxArtist;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ MusicSearchFoldersByArtist/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MusicFindArtist
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<string> _folderList = new List<string>();
+        private const string _root1 = @"\\NAS-QNAP\music\_COLLECTION";
+        private const string _root2 = @"\\NAS-QNAP\music_lossless\_COLLECTION";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            textBoxArtist.Enabled = false;
+            buttonSearch.Enabled = false;
+            listBox1.Enabled = false;
+            comboBox1.Enabled = false;
+
+            listBox1.Items.Clear();
+            _folderList.Clear();
+
+            textBoxArtist.Text = textBoxArtist.Text.Trim();
+            //textBoxArtist.Text = Utils.AddasteriskAtEnd(textBoxArtist.Text);
+
+            System.Windows.Forms.Application.DoEvents();
+
+            try
+            {
+
+                string baseArtist = Utils.RemoveDiacritics(textBoxArtist.Text, Utils.TextCaseAction.ToUpper);
+                if (baseArtist == null)
+                    throw new Exception("The artist is empty.");
+
+                string firstLetter = Utils.GetFirstLetter(baseArtist);
+                if (firstLetter == null)
+                    throw new Exception("First letter is invalid."); ;
+
+                //string aaa = RemoveDiacritics("eéêëèiïaâäàåcç  test");
+
+                string rootFolder = $@"{_root1}\{firstLetter}\";
+
+                GetDirectories(rootFolder, baseArtist, "MP3_", comboBox1.SelectedIndex);
+
+                rootFolder = $@"{_root2}\{firstLetter}\";
+                GetDirectories(rootFolder, baseArtist, "FLAC", comboBox1.SelectedIndex);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+
+            textBoxArtist.Enabled = true;
+            buttonSearch.Enabled = true;
+            listBox1.Enabled = true;
+            comboBox1.Enabled = true;
+            Cursor = Cursors.Default;
+
+            System.Windows.Forms.Application.DoEvents();
+        }
+
+        private void GetDirectories(string rootDirectoryPath, string baseArtist, string autioType, int searchType)
+        {
+            if (!Directory.Exists(rootDirectoryPath))
+                return;
+
+            string[] folderArray = Directory.GetDirectories(rootDirectoryPath);
+
+            foreach (string folderName in folderArray)
+            {
+                string shortName = folderName.Replace(rootDirectoryPath, "");
+
+                string name = Utils.RemoveDiacritics(shortName, Utils.TextCaseAction.ToUpper);
+
+                int pos = name.IndexOf(baseArtist);
+
+                bool addItem = false;
+                if (searchType == 0) // name*
+                    addItem = (pos == 0);
+
+                if (searchType == 1) // *name*
+                    addItem = (pos >= 0);
+                
+                if (addItem){
+                    listBox1.Items.Add(autioType + " : " + shortName);
+                    _folderList.Add(folderName);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = $"{this.Text} - Version: {System.Windows.Forms.Application.ProductVersion.ToString()}";
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            string folder = _folderList[listBox1.SelectedIndex];
+
+            Process.Start("explorer.exe", folder);
+        }
+    }
+}
